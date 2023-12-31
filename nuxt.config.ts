5d03ed2474ba3d686cb59657d6cb438cf6f329ee
김주호 <juho_kim@outlook.com>
2023-12-31 17:55:44 +0900

    nuxt 1)modules 2)routes 3)dev-timeline

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true
+    }
+  },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@formkit/auto-animate",
+    "@vueuse/nuxt",
+    "nuxt-vitest"
+  ]
 })
